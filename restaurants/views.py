356f8848d6775811be91fadb2a6aef9aee2af110
amal-nj <52827969+amal-nj@users.noreply.github.com>
2019-07-22 21:42:53 +0300

    Task3 done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+        'my_list':[
+            {"restaurant_name":"KFC", "food_type":"fast food"},
+            {"restaurant_name":"Kudo", "food_type":"fast food"},
+            {"restaurant_name":"Shawrmtk", "food_type":"fast food"}
+        ]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +19,7 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+        'my_object': {"restaurant_name":"wokx", "food_type":"chinese food"}
 
     }
     return render(request, 'detail.html', context)
